bf744015a6cd1f144d582b7e8e4b298255341501
Bharath Girirajan <83293682+Bharath0726@users.noreply.github.com>
2024-09-27 01:11:33 -0400

    Update TradeGPT.py logoUpdate3

--- TradeGPT.py
+++ TradeGPT.py
@@ -16,7 +16,7 @@ load_dotenv()
 # groq_api_key=os.getenv("GROQ_API_KEY")
 
 # Set page title and favicon (Ananda logo)
-logo_path = "https://imgur.com/mhcELv7"  # Replace with the path to your Ananda logo
+logo_path = "https://ananda.exchange/wp-content/uploads/2022/03/cropped-Fondos-y-recursos-20.png"  # Replace with the path to your Ananda logo
 st.set_page_config(
     page_title="ChatBot",
     page_icon=logo_path  # Setting the favicon (logo) in the tab
@@ -147,7 +147,7 @@ st.markdown(
 )
 
 # Display the logo at the top left corner using st.image
-logo_path = "https://imgur.com/mhcELv7"
+logo_path = "https://ananda.exchange/wp-content/uploads/2022/03/cropped-Fondos-y-recursos-20.png"
 st.image(logo_path, width=100)  # Adjust the width if needed
 
 # Initialize session state for chat history
